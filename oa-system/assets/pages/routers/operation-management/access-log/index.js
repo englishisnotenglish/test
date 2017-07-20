b7402debe65764200d6bd340770ed84696c057de
wjz <wjz@baifendian.com>
2017-07-20 11:27:54 +0800

    删除不需要

--- oa-system/assets/pages/routers/operation-management/access-log/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    path: '/operation-management/access-log',
-
-    getComponent(nextState, cb) {
-        require.ensure([], (require) => {
-            cb(null, require('./components/access_log.jsx'));
-        });
-    }
-};
