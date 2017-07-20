b1edeb5d886f190cee46537326f842a7ecfd4e44
wjz <wjz@baifendian.com>
2017-07-20 18:16:18 +0800

    init finished

--- tool_factory/src/containers/main.js
+++ tool_factory/src/containers/main.js
@@ -1,20 +1,49 @@
-import React, {Component} from 'react';
-import  {Router, Link} from 'react-router';
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom'
 
-const LinkWrapper = (props) => {
-    const Component = props.to ? Link : 'a';
-    return (
-        <Component {...props}>
-            { props.children }
-        </Component>
-    )}
+const Home = () => (
+    <div>
+        <h2>Home</h2>
+    </div>
+)
 
-class Main extends Component{
-    render(){
-        return (this.props.children ||
-        <LinkWrapper to={'old'} className="item">
-            sb
-        </LinkWrapper>);
-    }
-}
-export default Main;
+const About = () => (
+    <div>
+        <h2>About</h2>
+    </div>
+)
+
+const Topic = ({ match }) => (
+    <div>
+        <h3>{match.params.topicId}</h3>
+    </div>
+)
+
+const Topics = () => {
+                    return <div>
+                        你好
+                    </div>
+                };
+
+const BasicExample = () => (
+    <Router>
+        <div>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/topics">Topics</Link></li>
+            </ul>
+
+            <hr/>
+
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/topics" component={Topics}/>
+        </div>
+    </Router>
+)
+export default BasicExample
